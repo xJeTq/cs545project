6edbad8c36ef7c1fb45f8f8391f9b3a05aae9e22
xJeTq <anthonypetraccoro@yahoo.com>
2024-11-07 09:57:21 -0500

    commit

---
+++ geoexplorer/src/pages/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  console.log('About component rendered');
+  return (
+      <div>
+          <h1>About GeoExplorer.</h1>
+          <p>.</p>
+      </div>
+  );
+};
+
+export default About;
