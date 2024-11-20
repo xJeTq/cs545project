5e10a44c13991325719d618fb4bf66b98f701a87
xJeTq <anthonypetraccoro@yahoo.com>
2024-11-20 12:08:06 -0500

    Documentation
    Incorporated documentation in About page.

--- geoexplorer/src/pages/About.js
+++ geoexplorer/src/pages/About.js
@@ -17,6 +17,19 @@ const About = () => {
         GeoExplorer is an interactive web-based globe application designed to allow users to explore geographical data visually. Users can click on countries on the globe to view detailed information about each country, such as its name, capital and population. The globe is rendered using Three.js, and the country outlines are generated from public GeoJSON data. The application provides a smooth and intuitive user interface, allowing users to freely navigate around the globe.
       </p>
       <p style={{
+        backgroundColor: 'rgba(31, 31, 31, 0.9)',
+        color: 'white',
+        padding: '20px',
+        margin: '15px',
+        marginLeft: '250px',
+        marginRight: '250px',
+        fontSize: '16px',
+        lineHeight: '30px',
+        borderRadius: '8px'
+      }}>
+        To read documentation, <a href="https://drive.google.com/file/d/1j5V98T_Fc5M4qf08lKxGq8ckmuqds2id/view?usp=drive_link" style={{ color: 'white', textDecoration: 'underline' }}>click here</a>.
+      </p>
+      <p style={{
         position: 'absolute',
         bottom: '50px'
       }}>CS 545: Human Computer Interaction</p>
@@ -29,7 +42,6 @@ const About = () => {
         bottom: '10px'
       }}>Anthony Curcio-Petraccoro, Andrew Krasinski, Harrison Huston, Dean Zazzera</p>
     </div>
-
   );
 };
 
